a9026bc7268129328bffc69bc02e6fe0204a68b6
Joe Bevis <49538329+icebev@users.noreply.github.com>
2021-10-13 18:30:30 +0100

    split code, added JSON support, added new backgrounds

---
+++ canvasAnimation.js
@@ -0,0 +1,221 @@
+const canvas = {
+    width: 1600,
+    height: 900
+};
+
+// referencing thecanvas elements using the HTML ID
+const backCanvas = document.getElementById("backCanvas");
+const frontCanvas = document.getElementById("frontCanvas");
+const vfxCanvas = document.getElementById("vfxCanvas");
+// setting up the 2D canvas
+const backCtx = backCanvas.getContext('2d');
+const frontCtx = frontCanvas.getContext('2d');
+const vfxCtx = vfxCanvas.getContext('2d');
+// 16:9 aspect ratio to fit most modern monitors
+backCanvas.width = canvas.width;
+backCanvas.height = canvas.height;
+frontCanvas.width = canvas.width;
+frontCanvas.height = canvas.height;
+vfxCanvas.width = canvas.width;
+vfxCanvas.height = canvas.height * 0.7;
+
+// function called as part of the animation loop that adds the 'revealed' class to each of the adventure text span elements over time to make them visible
+function RevealSpanCharacters(revealList, deltatime) {
+    timeSinceLastCharacter += deltatime;
+    if (timeSinceLastCharacter > characterRevealDelay && revealList.length > 0) {
+        var spanToReveal = revealList.splice(0, 1)[0];
+        spanToReveal.span.classList.remove('hidden');
+        spanToReveal.span.classList.add('revealed');
+        timeSinceLastCharacter = 0 + (timeSinceLastCharacter % 100);
+    // hide the skip button and display choice buttons if all the text is visible   
+    } else if (revealList.length === 0 && currentTextNode.nodeId) {
+        skipButtonElement.style.visibility = "Hidden";
+        DisplayChoiceButtons(currentTextNode);
+    };
+};
+
+// function to display the relevant buttons once all adventure text is revealed
+function DisplayChoiceButtons(textNode) {
+    // if there are no dialogue choices to display, display the continue button instead
+    if (!currentTextNode.dialogueChoices) {
+        continueButtonElement.style.visibility = "Visible";
+        continueButtonElement.onclick = function() {SelectContinue(textNode)};
+    } else {
+        dialogueChoicesElement.style.visibility = "Visible";
+    }; 
+};
+
+
+// drawing an energy bar on the canvas with the appearance of a battery
+function DrawEnergyBar() {
+    let centerLineX = vfxCanvas.width / 2;
+    let batteryWidth = maxEnergyLevel * 2;
+    let batteryHeight = 75;
+    let batteryX = centerLineX - batteryWidth / 2;
+    let batteryY = 100;
+    // translucent back
+    vfxCtx.fillStyle = "rgb(255, 255, 255, 0.4)";
+    vfxCtx.fillRect(centerLineX - batteryWidth / 2, batteryY, batteryWidth, batteryHeight);
+    // alter player sprite mood and battery fill color based on the energy level
+    switch (true) {
+        case energyLevel <= 20:
+            changePlayerMood('sad');
+            vfxCtx.fillStyle = "red";
+            break;
+        case energyLevel <= 70:
+            changePlayerMood();
+            vfxCtx.fillStyle = "yellow";
+            break;
+        default:
+            changePlayerMood('happy');
+            vfxCtx.fillStyle = "green";
+    };
+    vfxCtx.fillRect(batteryX, batteryY, energyLevel * 2, batteryHeight);
+    // add a numeric energy indicator and a descriptive label on top
+    vfxCtx.font = '35px Impact';
+    vfxCtx.fillStyle = "black";
+    vfxCtx.fillText(energyLevel, centerLineX + 20, batteryY + 50);
+    vfxCtx.fillText('Energy Level:', batteryX, batteryY - 20);
+    // draw the black outline on top of the energy bar and back
+    vfxCtx.beginPath();
+    vfxCtx.rect(batteryX, batteryY, batteryWidth, batteryHeight);
+    vfxCtx.rect(batteryX + batteryWidth , batteryY + batteryHeight / 2 - 10, 6, 20);
+    vfxCtx.lineWidth = "6";
+    vfxCtx.stroke();
+};
+
+// function that switches the sprite animation Y frame to reflect current mood
+function changePlayerMood(mood) {
+    switch (mood) {
+        case 'sad':
+            batteryborn.frameY = 1;
+            break;
+        case 'happy':
+            batteryborn.frameY = 2;
+            break;
+        default:
+            batteryborn.frameY = 0;
+            break;
+    };
+};
+
+// if the transistion opacity value has been set it will slowly return to fully transparent
+function CanvasTransitionUpdate(deltatime) {
+    if (transitionOpacity >= 0) {
+        frontCanvas.style.visibility = "Visible";
+        frontCtx.fillStyle = "rgb(0, 0, 0," + transitionOpacity + ")";
+        frontCtx.fillRect(0, 0, canvas.width, canvas.height);
+        transitionOpacity -= (deltatime * 0.0005);
+    } else {
+        frontCanvas.style.visibility = "Hidden";
+    };
+};
+
+
+
+// a generic character class for efficient sprite configuration
+class Character {
+    constructor(x, y, imageSrc, spriteW, spriteH, scale, frames) {
+        this.x = x;
+        this.y = y;
+        this.image = new Image();
+        this.image.src = imageSrc;
+        this.spriteWidth = spriteW;
+        this.spriteHeight = spriteH;
+        this.scale = scale;
+        this.frameX = 0;
+        this.frameY = 0;
+        this.maxFramesX = frames - 1;
+        this.timeSinceLastFrame = 0;
+        this.frameInterval = 500;
+        this.markedForDeletion = false;
+    };
+    // update and draw methods will be called every time the screen refreshes and may increment the animation frame
+    update(deltatime) {
+        this.timeSinceLastFrame += deltatime;
+        if (this.timeSinceLastFrame >= this.frameInterval) {
+            if (this.frameX < this.maxFramesX) {
+                this.frameX++;
+            } else {
+                this.frameX = 0;
+            };
+            this.timeSinceLastFrame = 0 + (this.timeSinceLastFrame % this.frameInterval);
+        };
+    };
+    draw() {
+        backCtx.drawImage(this.image, this.spriteWidth * this.frameX, this.spriteHeight * this.frameY, this.spriteWidth, this.spriteHeight, this.x, this.y, this.spriteWidth * this.scale, this.spriteHeight * this.scale);
+    };
+};
+
+// a generic animated back class for efficient sprite configuration
+class Animatedback {
+    constructor(imageSrc, spriteW, spriteH, frames) {
+        this.image = new Image();
+        this.image.src = imageSrc;
+        this.spriteWidth = spriteW;
+        this.spriteHeight = spriteH;
+        this.frameX = 0;
+        this.maxFramesX = frames - 1;
+        this.timeSinceLastFrame = 0;
+        this.frameInterval = 1000;
+        this.markedForDeletion = false;
+    }
+    // update and draw methods will be called every time the screen refreshes and may increment the animation frame
+    update(deltatime) {
+        this.timeSinceLastFrame += deltatime;
+        if (this.timeSinceLastFrame >= this.frameInterval) {
+            if (this.frameX < this.maxFramesX) {
+                this.frameX++;
+            } else {
+                this.frameX = 0;
+            };
+            this.timeSinceLastFrame = 0 + (this.timeSinceLastFrame % this.frameInterval);
+        };
+    }
+    draw() {
+        backCtx.drawImage(this.image, this.spriteWidth * this.frameX, 0, this.spriteWidth, this.spriteHeight, 0, 0, canvas.width, canvas.height);
+    }
+};
+
+var backgroundImage = new Image("img/BackgroundSheet.png");
+backgroundImage.src = "img/BackgroundSheet.png";
+backgroundManager = {
+    backgrounds: backgroundImage,
+    spriteWidth: 480,
+    spriteHeight: 270,
+    frameX: 0,
+    frameY: 0,
+
+    draw() {
+        backCtx.drawImage(this.backgrounds, this.spriteWidth * this.frameX, this.spriteHeight * this.frameY, this.spriteWidth, this.spriteHeight, 0, 0, backCanvas.width, backCanvas.height);
+    }
+};
+
+backgrounds = new Image("BackgroundSheet.png")
+backCtx.drawImage(backgrounds, this.spriteWidth * this.frameX, 0, this.spriteWidth, this.spriteHeight, 0, 0, canvas.width, canvas.height);
+batteryborn = new Character(5, 150, "img/batteryborn.png", 180, 180, 3, 2);
+junkyard = new Animatedback("img/junkyard.png", 1920, 1080, 2);
+// animation loop
+function Animate(timestamp) {
+    let deltatime = timestamp - lastFrameTime;
+    lastFrameTime = timestamp;
+    backCtx.clearRect(0, 0, canvas.width, canvas.height);
+    frontCtx.clearRect(0, 0, canvas.width, canvas.height);
+    vfxCtx.clearRect(0, 0, canvas.width, vfxCanvas.height);
+    junkyard.update(deltatime);
+    junkyard.draw();
+    backgroundManager.draw();
+    batteryborn.update(deltatime);
+    batteryborn.draw();
+    DrawEnergyBar();
+    if (!gameStarted) {
+        transitionOpacity = 1.5;
+    }    
+    CanvasTransitionUpdate(deltatime);
+    RevealSpanCharacters(characterSpans, deltatime);
+    requestAnimationFrame(Animate);
+};
+
+
+// begin the game animation loop
+window.onload = Animate(0);
---
+++ dialogueSystem.js
@@ -0,0 +1,259 @@
+﻿//// DIALOGUE SYSTEM CODE FOR VOLTAIC ////
+
+// retrieve the adventure text from the JSON file using AJAX
+var adventureTextNodes = [];
+const xmlhttp = new XMLHttpRequest();
+xmlhttp.onload = function() {
+  adventureTextNodes = JSON.parse(this.responseText);
+  };
+xmlhttp.open("GET", "adventureText.json", true);
+xmlhttp.send();
+
+// declare key global variables for use throughout the code
+var _playerName = '';
+var currentTextNode = {};
+var gameStarted = false;
+
+// variables for adaptive energy level and inventory system
+var energyLevel = 50;
+var maxEnergyLevel = 100;
+var inventory = [];
+
+// variables to keep track of animations and typewriter effect
+var lastFrameTime = 0;
+var timeSinceLastCharacter = 0;
+var characterSpans = [];
+var characterRevealDelay = 80;
+var transitionOpacity = 1;
+
+// constant variables used for referring to changing HTML elements
+const adventureTextContainerElement = document.getElementById("adventureTextContainer");
+const dialogueChoicesElement = document.getElementById("dialogueChoices");
+const namePlateLeftElement = document.getElementById("namePlateLeft");
+const namePlateRightElement = document.getElementById("namePlateRight");
+const continueButtonElement = document.getElementById("continueButton");
+const skipButtonElement = document.getElementById("skipButton");
+
+// function that checks the player name entered
+function NameEntrySubmit() {
+    let nameWarningElement = document.getElementById("nameWarning");
+    let enteredName = document.getElementById("nameInput").value;
+    if (enteredName.length > 8) {
+        nameWarningElement.innerHTML = "Enter a shorter name.";
+    } else if (enteredName) {
+        // if the name is acceptable store it and start the game
+        _playerName = enteredName;
+        namePlateLeftElement.innerHTML = _playerName;
+        // switch the visible elements
+        document.getElementById("nameEntryContainer").style.visibility = "Hidden";
+        document.getElementById("dialogueChoicesContainer").style.visibility = "Visible";
+        adventureTextContainerElement.style.visibility = "Visible";
+        gameStarted = true;
+        StartGame();
+    } else {
+        nameWarningElement.innerHTML = "Enter a name.";
+    };
+};
+
+// function called when the game should begin or restart
+function StartGame() {
+    UpdateGameText(1);
+    energyLevel = 50;
+    inventory = [];
+    transitionOpacity = 1.5;
+};
+
+// large function that takes in the target node ID to change what is being displayed
+function UpdateGameText(targetNodeId) {
+    // find the specified text node inside the array literal using the target node ID
+    let targetTextNode = adventureTextNodes.find(textNode => textNode.nodeId === targetNodeId);
+    // if successful the current text node becomes the original target
+    if (targetTextNode) {
+        currentTextNode = targetTextNode;
+        console.log("Text node retrieved: " + currentTextNode.nodeId);
+    } else {
+        console.log("Failed to retrieve a node with ID: " + targetNodeId);
+        return false;
+    };    
+    // update the main adventure text, reveal speed, character nameplates and dialogue option buttons passing properties of the retrieved node
+    ChangeTextSpeed(currentTextNode.textSpeed);
+    ChangeAdventureText(currentTextNode.text);
+    UpdateNamePlates(currentTextNode.speaker);
+    CreateChoiceButtons(currentTextNode.dialogueChoices);
+};
+
+// called before new text is displayed or when the skip button is clicked and changes the character delay value in ms
+function ChangeTextSpeed(speed) {
+    switch (speed) {
+        case "slow":
+            characterRevealDelay = 100;
+            break;
+        case "fast":
+            characterRevealDelay = 50;
+            break;
+        case "superfast":
+            characterRevealDelay = 10;
+            break;    
+        default:
+            characterRevealDelay = 80;
+            break;
+    };
+};
+
+// function that will display the adventure text with a typewriter effect by first creating invisible span elements for each letter
+function ChangeAdventureText(inputString) {
+    // remove any old text first
+    while (adventureTextContainerElement.firstChild) {
+        adventureTextContainerElement.removeChild(adventureTextContainerElement.firstChild);
+    };
+    // show the skip button when the text is changed and hide any dialogue choices
+    skipButtonElement.style.visibility = "Visible";
+    dialogueChoicesElement.style.visibility = "Hidden";
+    // iterate through each word of the input string to include the player name if applicable
+    let words = inputString.split(" ");
+    for (let i = 0; i < words.length; i++) {
+        if (words[i] === "_playerName") {
+            words[i] = _playerName;
+        };
+    };
+    let revivedString = words.join(" ");
+    // set the array of span elements back to empty before adding the new hidden ones
+    characterSpans = [];
+    revivedString.split("").forEach((character) => {
+        let newSpan = document.createElement("span");
+        newSpan.innerHTML = character;
+        newSpan.classList.add("hidden");
+        adventureTextContainerElement.appendChild(newSpan);
+        characterSpans.push({
+           span: newSpan,
+        });
+    });
+};
+
+// show or hide the dialogue name plates based on the current speaker
+function UpdateNamePlates(speaker) {
+    switch (speaker) {
+        case "player":
+            namePlateLeftElement.style.visibility = "Visible";
+            namePlateRightElement.style.visibility = "Hidden";
+            break;
+        default:
+            if (speaker) {
+                namePlateRightElement.innerHTML = speaker
+                namePlateLeftElement.style.visibility = "Hidden";
+                namePlateRightElement.style.visibility = "Visible";
+            } else {
+                namePlateLeftElement.style.visibility = "Hidden";
+                namePlateRightElement.style.visibility = "Hidden";
+            };
+    };
+};
+
+function CreateChoiceButtons(dialogueChoices) {
+    // clear any previous dialogue choices by removing the dialogue option buttons entirely
+    while (dialogueChoicesElement.firstChild) {
+        dialogueChoicesElement.removeChild(dialogueChoicesElement.firstChild);
+    };
+    // create new button elements that respond to being clicked
+    if (dialogueChoices) {
+        dialogueChoices.forEach(dialogueChoice => {
+            // before displaying the choice check whether the requirements are met
+            if (CheckChoiceRequirements(dialogueChoice)) {
+                let newButton = document.createElement("button");
+                newButton.innerText = dialogueChoice.text;
+                newButton.classList.add("genericButton");
+                newButton.classList.add("choiceButton");
+                newButton.onclick = function() {SelectChoice(dialogueChoice)};
+                dialogueChoicesElement.appendChild(newButton);
+            };
+        });
+    };
+};
+
+// function to check any prerequisistes for the dialogue choice and return true only if they are met
+function CheckChoiceRequirements(dialogueChoice) {
+    // check inventory requirements
+    if (dialogueChoice.inventoryRequired) {
+        for (let i = 0; i < dialogueChoice.inventoryRequired.length; i++) {
+            let requiredItem = dialogueChoice.inventoryRequired[i];
+            if (!inventory.includes(requiredItem)) {
+                console.log(`The required item ${requiredItem} for node ${currentTextNode.nodeId} dialogue choice has not been acquired.`)
+                return false;
+            };
+        };       
+    }; 
+    // check energy level requirements
+    if (dialogueChoice.energyLevelRequired) {
+        if (dialogueChoice.energyLevelRequired > energyLevel) {
+            console.log(`The ${dialogueChoice.energyLevelRequired} energy required by node ${currentTextNode.nodeId} dialogue choice exceeds current energy.`);
+            return false  
+        };
+    };
+    return true;       
+}; 
+
+// the continue button skips to the desired node if applicable 
+function SelectContinue(currentNode) {
+    continueButtonElement.style.visibility = "Hidden";
+    if (currentNode.skipToNode) {
+        UpdateGameText(currentNode.skipToNode);
+    } else {
+        UpdateGameText(currentNode.nodeId + 1);
+        console.log("No skipToNode specified.")
+    };
+};
+
+// function to execute whenever a dialogue choice is selected taking that dialogue choice object as a parameter
+function SelectChoice(dialogueChoiceSelected) {
+    // alter the inventory and update energy level based on the choice
+    UpdateInventory(dialogueChoiceSelected);
+    UpdateEnergy(dialogueChoiceSelected);
+    // retrieve the next target node from the dialogue choice object
+    let nextTargetNodeId = dialogueChoiceSelected.nextText;
+    // restart the game if a value of 0 is received, otherwise continue to the target node
+    if (nextTargetNodeId === 0) {
+        return StartGame();
+    } else {
+        UpdateGameText(nextTargetNodeId);
+    };
+};
+
+// function to update the inventory array whenever a dialogue choice is selected
+function UpdateInventory(dialogueChoice) {
+    if (dialogueChoice.inventoryAdd) {
+        dialogueChoice.inventoryAdd.forEach((itemToAdd) => AddToInventory(itemToAdd));
+    };
+    if (dialogueChoice.inventoryRemove) {
+        dialogueChoice.inventoryRemove.forEach((itemToRemove) => RemoveFromInventory(itemToRemove));
+    };
+    console.log("Current inventory: " + inventory);
+};
+
+// function to add a string to the inventory array
+function AddToInventory(stringToAdd) {
+    inventory.push(stringToAdd);
+};
+
+// function to remove a string from the inventory array
+function RemoveFromInventory(stringToRemove) {
+    for (let i = 0; i < inventory.length; i++) {
+        if (inventory[i] === stringToRemove) {
+            inventory.splice(i, 1);
+            // return true once an item has been removed to avoid removing multiple of the same item
+            return true;
+        };
+    };
+};
+
+// function to alter the player energy level if the dialogue choice object includes an energyChange
+function UpdateEnergy(dialogueChoice) {
+    if (Math.abs(dialogueChoice.energyChange)) {
+        energyLevel += dialogueChoice.energyChange;
+        console.log(`Energy level has been changed by ${dialogueChoice.energyChange} to a new value of ${energyLevel}`);
+        if (energyLevel > maxEnergyLevel) {
+            console.log(`Maximum energy level was exceeded by ${energyLevel - maxEnergyLevel}!`)
+            energyLevel = 100;
+        }
+    };
+};
+
